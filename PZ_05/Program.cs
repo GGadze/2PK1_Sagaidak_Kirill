8dd4e4f0634db37f52a6a86d76fd28aefe694ace
Кирилл Сагайдак <rbhbkkgjxnf@gmail.com>
2023-09-23 01:05:15 +0500

    добавил 5 практику

---
+++ PZ_05/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = -4.0;
+            double h = 0.5;
+            double y;
+
+            while (x <= 4)
+            {
+
+                Console.Write($"x: {x} ");
+                if (x == Math.Truncate(x))
+                {
+                    Console.Write("  ");
+                }
+                Console.Write("| ");
+                y = Math.Abs(x);
+                Console.WriteLine($"y: {y}");
+                Console.WriteLine();
+                x += h;
+            }
+
+
+        }
+    }
+}
