299a05e629b583418cb4dfc2bb7d25b09224c472
Кирилл Сагайдак <rbhbkkgjxnf@gmail.com>
2023-11-23 12:49:36 +0500

    добавил 10 практику

---
+++ PZ_10/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PZ_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст: ");
+            string str = Console.ReadLine();
+
+            bool flag = true;
+
+            while (flag)
+            {
+                Console.WriteLine("Можете выйти из программы и вывести полученный текст (введите exit)\n" +
+                    " или добавить предложения к своему тексту! Введите их здесь:");
+                string new_str = Console.ReadLine();
+
+                if (new_str == "exit")
+                    flag = false;
+                else
+                    str += " " + new_str;
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
