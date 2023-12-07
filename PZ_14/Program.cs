9ddaad82b2fcfb751bfb12b4080ce63051fd81a3
Кирилл Сагайдак <rbhbkkgjxnf@gmail.com>
2023-12-07 13:30:49 +0500

    добавил 14  и 15 практику

---
+++ PZ_14/Program.cs
@@ -0,0 +1,67 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System;
+
+namespace PZ_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"test.txt";
+            string line = null;
+
+            Console.WriteLine("Введите построчно содержимое файла (чтобы закончить, введите stop):");
+
+            using (FileStream file = new FileStream(path, FileMode.Create, FileAccess.ReadWrite))
+            {
+                using (StreamWriter writer = new StreamWriter(file))
+                {
+
+                    while (line != "stop")
+                    {
+                        writer.WriteLine(line);
+                        line = Console.ReadLine();                      
+                    }
+                }
+            }
+
+            string[] lines = File.ReadAllLines(path);
+
+            for (int i = 0; i < lines.Length - 1; i++)
+            {
+                string str = null;
+                int count = i + 1;
+                while (count != 0)
+                {
+                    if (lines[count].Length > lines[count - 1].Length)
+                    {
+                        str = lines[count];
+                        lines[count] = lines[count - 1];
+                        lines[count - 1] = str;
+                    }
+                    count--;
+                }
+            }
+
+            Console.WriteLine("Перезаписать файл, упорядочив его по возрастанию длины строк? (yes/no)");
+            string yes_no = Console.ReadLine();
+            if (yes_no == "yes")
+            {
+                File.Delete(path);
+                using (FileStream file = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                {
+                    using (StreamWriter writer = new StreamWriter(file))
+                    {
+
+                        for (int i = 0; i < lines.Length; i++)
+                        {
+                            writer.WriteLine(lines[i]);
+                        }
+                    }
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ PZ_15/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace PZ_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+
+            while (flag)
+            {
+                flag = false;
+                Console.WriteLine("Введите полный путь к каталогу:");
+                string path_cat = Console.ReadLine();
+
+                if (Directory.Exists(path_cat)) //проверка на существование каталога
+                {
+                    DirectoryInfo dir = new DirectoryInfo(path_cat);
+
+                    string[] files = Directory.GetFiles(path_cat);
+                    int count = 0;
+
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        if (files[i].Substring(files[i].LastIndexOf(".")) == ".exe") //наполнение массива всеми файлами каталога
+                                                                                     //и вычисление количества .exe файлов в каталоге
+                            count++;
+                    }
+
+                    string[] files_exe = new string[count];  //создание массива размерностью, соответствующей количеству .exe файлов в каталоге
+
+                    count = 0;
+
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        if (files[i].Substring(files[i].LastIndexOf(".")) == ".exe")
+                        {
+                            files_exe[count] = files[i]; //наполнение массива .exe файлами из массива со всемм файлами
+                            count++;
+                        }
+                    }
+
+                    foreach (string file in files_exe)
+                    {
+                        Console.WriteLine($"Название файла: {file.Substring(file.LastIndexOf(@"\") + 1)}");
+                        Console.WriteLine($"Время создания файла: {Directory.GetCreationTime(file)}");
+                    }
+                }
+                else
+                    flag = true;
+                    Console.WriteLine("Директории с таким названием не существует :( Попробуйте еще раз :<");
+            }
+
+        }
+    }
+}
